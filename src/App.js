c98843b6aa2b5fa600de4d55491c6c8db2323cf3
DanielSoaresRocha <legoliga123@gmail.com>
2020-06-15 11:06:06 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App () {
+  const logo = 'https://www.projetoweb.com.br/sites-padrao-prontos-simples-baratos-lancamentos/'
+    + 'padrao6-programado/template/pw-images/logo.png'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="containerHome">
+      <header>
+        <img src={logo} alt='Logo Engemac' />
+        <div className='space'>
+        </div>
+        <div className='menu'>
+          <p>HOME</p>
+          <p>SOBRE</p>
+          <p>SERVIÇOS</p>
+          <p>PORTFÓLIO</p>
+          <p>CONTATO</p>
+        </div>
       </header>
+
+      <section>
+        <img alt='Terraplanagem' src='https://www.kannaterraplenagem.com.br/arquivos/31975-d1cef-terraplanagem-canarana-mt.jpg' />
+        <div className='about'>
+          <h1>Engemac</h1>
+          <h3>Aqui ficará uma breve descrição sobre a empresa </h3>
+        </div>
+      </section>
     </div>
   );
 }
